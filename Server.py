aaccbf0dcc16198cf819ba479df50c085ba6ce2c
Thilanga Ruwinath <thilangaruwinath@gmail.com>
2024-12-15 12:31:28 +0530

    Final Update Server.py

--- Server.py
+++ Server.py
@@ -1,11 +1,10 @@
 import threading
 import socket
 
-
-host = '127.0.0.1'    #Local host
+host = '127.0.0.1'    # Local host
 port = 55555
 
-server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen()
 
@@ -19,31 +18,30 @@ def broadcast(message):
 def handle(client):
     while True:
         try:
-            message =client.recv(1024)
+            message = client.recv(1024)
             broadcast(message)
         except:
-            index = client.index(client)
-            client.remove(client)
+            index = clients.index(client)
+            clients.remove(client)
             client.close()
-            nicknames = nicknames[index]
-            broadcast(f'{nicknames}left the chat !'.encode('ascii')) 
-            nicknames.remove(nicknames)
+            nickname = nicknames[index]
+            broadcast(f'{nickname} left the chat!'.encode('ascii'))
+            nicknames.remove(nickname)
             break
-
 
 def receive():
     while True:
         client, address = server.accept()
-        print(f"Connected with{str(address)}")
+        print(f"Connected with {str(address)}")
         
         client.send('NICK'.encode('ascii'))
         nickname = client.recv(1024).decode('ascii')
         nicknames.append(nickname)
         clients.append(client)
 
-        print(f'Nickname of the client is{nickname}')
-        broadcast(f'{nickname}joined the chat!'.encode(ascii))
-        client.send('Connected to the server !'.encode('ascii'))
+        print(f'Nickname of the client is {nickname}') 
+        broadcast(f'{nickname} joined the chat!'.encode('ascii'))
+        client.send('Connected to the server!'.encode('ascii'))
 
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
