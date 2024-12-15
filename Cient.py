19319c436760f59502311580490a493d3ba483dd
Thilanga Ruwinath <thilangaruwinath@gmail.com>
2024-12-15 12:31:03 +0530

    Final Update Cient.py

--- Cient.py
+++ Cient.py
@@ -1,10 +1,10 @@
 import socket
 import threading
 
-nickname = input("Choose a nickname :")
+nickname = input("Choose a nickname: ")
 
-client = socket.socket(socket.AF_INE, socket.SOCK_STREAM)
-client.connect(('127.0.0.1',55555))
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 55555))
 
 def receive():
     while True:
@@ -15,18 +15,17 @@ def receive():
             else:
                 print(message)
         except:
-            print("An error occured !")
+            print("An error occurred!")
             client.close()
             break
 
 def write():
     while True:
-        message = f'{nickname}:{input("")}'
+        message = f'{nickname}: {input("")}'
         client.send(message.encode('ascii'))
 
-receive_thread=threading.Thread(target=receive)
+receive_thread = threading.Thread(target=receive)
 receive_thread.start()
 
 write_thread = threading.Thread(target=write)
 write_thread.start()
-
